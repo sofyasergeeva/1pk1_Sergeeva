72cf942a375833ba6e305b4020c50cc39f353ce3
petrovich.kruglyash@mail.ru <petrovich.kruglyash@mail.ru>
2025-01-26 21:52:22 +0500

    занятие 3, задача 8.

---
+++ Task_03_08/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_03_08
+{
+    internal class Program
+    { /* Даны натуральные числа от 20 до 50. Напечатать те из них, которые делятся на 3, но не делятся на 5
+       */
+        static void Main(string[] args)
+           
+           
+
+          
+        {
+            for (int i = 20; i <= 50; i++)
+            {
+                if (i % 3 == 0 && i % 5 != 0)
+                    Console.WriteLine($"{i}");
+            }
+        }
+
+        
+
+
+
+    }
+    
+}
