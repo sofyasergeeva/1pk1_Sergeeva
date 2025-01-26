d0d000e78a52ae867ed6f6c494d5ed433caf0fd0
petrovich.kruglyash@mail.ru <petrovich.kruglyash@mail.ru>
2025-01-26 21:14:56 +0500

    занятите 3, задание 4.

---
+++ Task_03_04/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Task_03_04
+{
+    internal class Program
+    {/* Пользователь вводить в консоли произвольный текст. После каждого ввода консоль очищается. Когда
+         * пользователь вводить слово «exit» или пустую строку – ввод останавливается и выводиться количество строк
+         * введенных пользователем.
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите произвольный текст.");
+
+            // введенные строки
+            int count = 0;
+            
+            while (true)
+            {
+                count++;
+                Console.Clear();
+                string text = Console.ReadLine();
+
+                // прекращение считывания с консоли
+                if (text == "exit" || text == "")
+                {
+                    count -= text == "" ? 1 : 0;
+                    Console.WriteLine($"Количество введенных строк:{count} ");
+                    break;
+                }
+            }
+        }
+    }
+}
