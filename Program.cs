d9abe3987d217d05e32ac87019396b1c89b3bc95
petrovich.kruglyash@mail.ru <petrovich.kruglyash@mail.ru>
2025-01-26 20:11:29 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _1pk1_Sergeeva
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
