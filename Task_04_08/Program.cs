27f272c89be917e2d821a57509abfacc4750908b
petrovich.kruglyash@mail.ru <petrovich.kruglyash@mail.ru>
2025-01-26 22:45:37 +0500

    занятие 4, задание 8.

---
+++ Task_04_08/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task_04_08
+{
+    internal class Program
+    {/*Дан массив, содержащий последовательность 50 случайных чисел. Найти количество пар элементов, значения
+       которых равны.*/
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            // массив рандомных чисел
+            int[] mas = new int[50]; 
+          
+            // заполнение массива числами
+            for (int i = 0; i < mas.Length; i++)
+                mas[i] = rnd.Next(0, 10);
+
+            int count = 0;
+            for (int i = 1; i < mas.Length; i++)
+                if (mas[i - 1] == mas[i])
+                    count++;
+            Console.WriteLine($"В массиве {count} пар элементов, значения которых равны.");
+        }
+    }
+}
