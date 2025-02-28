997588891009620c8d4777711305b5af64867550
petrovich.kruglyash@mail.ru <petrovich.kruglyash@mail.ru>
2025-02-28 17:49:48 +0500

    задание 6 выполненно (не поняла где и что писать в ///)

---
+++ Task_06/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Task_10_06
+{
+    internal class Program
+    {/*Создать Метод ArrayGeneration не возвращающий значения,
+      *принимает целое число n, выводит на консоль сгенерированный
+      *массив размерности n*n.*/
+        static void Main()
+        {
+            Console.Write("Введите размерность массива n: ");
+            int n = int.Parse(Console.ReadLine());
+            ArrayGeneration(n);
+        }
+
+        static void ArrayGeneration(int n)
+        {
+            // Создаем двумерный массив размерностью n*n
+            int[,] array = new int[n, n];
+
+            // Заполняем массив случайными числами (например, от 1 до 100)
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    array[i, j] = random.Next(1, 101); // Генерация случайного числа от 1 до 100
+                }
+            }
+            
+
+            // Выводим массив на консоль
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+    
+
