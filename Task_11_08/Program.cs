1c631a7f73280af3208bde729224cab1efb1732f
petrovich.kruglyash@mail.ru <petrovich.kruglyash@mail.ru>
2025-02-28 18:56:23 +0500

    занятие 11, задание 8

---
+++ Task_11_08/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Task_11_08
+{
+    internal class Program
+    { /*Использование params и out: Напишите метод, который принимает переменное количество
+        чисел и возвращает их сумму и максимальное значение через выходные параметры (out).*/
+
+        static void Main(string[] args)
+        {
+            int sum;
+            int max;
+
+           
+            Proces(out sum, out max, 1, 2, 3, 4, 5);
+
+            // Вывод результатов
+            Console.WriteLine($"Сумма: {sum}, Максимальное значение: {max}");
+        }
+
+        static void Proces(out int sum, out int max, params int[] numbers)
+        {
+            sum = 0;
+            max = int.MinValue;
+
+            foreach (var number in numbers)
+            {
+                sum += number;
+
+                if (number > max)
+                {
+                    max = number;
+                }
+            }
+
+        }
+    }
+}
