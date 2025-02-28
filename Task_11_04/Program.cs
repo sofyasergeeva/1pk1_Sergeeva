0420366d2949ebb958a2142bce11f5f54fb2b83c
petrovich.kruglyash@mail.ru <petrovich.kruglyash@mail.ru>
2025-02-28 18:34:19 +0500

    занятие 11, задание 4

---
+++ Task_11_04/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task_11_04
+{
+    internal class Program
+    { /*Массив параметров (params): Напишите метод, который принимает массив чисел и возвращает
+        их среднее значение. Используйте ключевое слово params*/
+
+        static void Main(string[] args)
+        {
+            // Пример использования метода
+            double average = Numbers(1, 2, 3, 4, 5);
+            Console.WriteLine($"Среднее значение: {average}");
+        }
+
+        static double Numbers(params double[] numbers)
+        {
+            if (numbers.Length == 0)
+            {
+                throw new ArgumentException("Массив чисел не может быть пустым");
+            }
+
+            double sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+
+            return sum / numbers.Length;
+
+        }
+
+    }
+}
