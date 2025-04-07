2a35f45c35e784539481ca9ccf944cd7329de492
petrovich.kruglyash@mail.ru <petrovich.kruglyash@mail.ru>
2025-04-07 22:44:22 +0500

    занятие 20, задание 4

---
+++ Task_20_04/Program.cs
@@ -0,0 +1,55 @@
+﻿using static Task_20_04.VehicleTracker;
+
+namespace Task_20_04
+{
+    internal class Program
+    {/*Создайте программу для учета транспортных средств. Используйте перечисление VehicleType:
+        • Car
+        • Bike
+        • Bus
+        • Truck
+        • Motorcycle
+        Храните список транспортных средств (можно просто List).
+        Добавьте метод для подсчёта транспорта определённого типа (например, сколько грузовиков). 
+        Реализуйте поиск по типу и вывод информации*/
+        static void Main(string[] args)
+        {
+            // Список транспортных средств
+            List<Vehicle> vehicles = new List<Vehicle>();
+
+            // Добавление транспортных средств
+            vehicles.Add(new Vehicle(VehicleType.Car));
+            vehicles.Add(new Vehicle(VehicleType.Bike));
+            vehicles.Add(new Vehicle(VehicleType.Bus));
+            vehicles.Add(new Vehicle(VehicleType.Truck));
+            vehicles.Add(new Vehicle(VehicleType.Motorcycle));
+            vehicles.Add(new Vehicle(VehicleType.Truck));
+
+            // Подсчёт и вывод информации о грузовиках
+            int truckCount = CountVehiclesByType(vehicles, VehicleType.Truck);
+            Console.WriteLine($"Количество грузовиков: {truckCount}");
+
+            // Поиск и вывод всех транспортных средств определённого типа
+            VehicleType searchType = VehicleType.Truck; // Можно изменить на любой другой тип
+            var foundVehicles = FindVehiclesByType(vehicles, searchType);
+
+            Console.WriteLine($"Транспортные средства типа {searchType}:");
+            foreach (var vehicle in foundVehicles)
+            {
+                Console.WriteLine(vehicle);
+            }
+        }
+
+        // Метод для подсчёта транспортных средств определённого типа
+        static int CountVehiclesByType(List<Vehicle> vehicles, VehicleType type)
+        {
+            return vehicles.Count(v => v.Type == type);
+        }
+
+        // Метод для поиска транспортных средств определённого типа
+        static List<Vehicle> FindVehiclesByType(List<Vehicle> vehicles, VehicleType type)
+        {
+            return vehicles.Where(v => v.Type == type).ToList();
+        }
+    }
+}
---
+++ Task_20_04/VehicleTracker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_20_04
+{
+    internal class VehicleTracker
+    {
+        // Перечисление типов транспортных средств
+        public enum VehicleType
+        {
+            Car,
+            Bike,
+            Bus,
+            Truck,
+            Motorcycle
+        }
+
+        // Класс для представления транспортного средства
+        public class Vehicle
+        {
+            public VehicleType Type { get; set; }
+
+            public Vehicle(VehicleType type)
+            {
+                Type = type;
+            }
+
+            public override string ToString()
+            {
+                return Type.ToString();
+            }
+        }
+    }
+}
