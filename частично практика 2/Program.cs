0cf84819c05a24fa06d5a5b6a10d55cd34e80270
petrovich.kruglyash@mail.ru <petrovich.kruglyash@mail.ru>
2025-05-04 22:11:31 +0500

    частично практика без метода генератора

---
+++ частично практика 2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace частично_практика_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Info info = new(Category.Credit, 1000);
+            Bank.CreateUser("Сергеев Дмитрий Сергеевич");
+            Bank.Account(info, "Сергеев Дмитрий Сергеевич");
+            Bank.PrintClients();
+
+        }
+    }
+}
---
+++ частично практика 2/bank.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace частично_практика_2
+{
+    internal static class Bank
+    {
+        public static Dictionary<string, List<Info>> users = new();
+        /// <summary>
+        /// создание пользователя
+        /// </summary>
+        /// <param name="fullname"></param>
+        public static void CreateUser(string fullname)
+        {
+            users[fullname] = new();
+            //Console.WriteLine(
+        }
+        /// <summary>
+        /// создание счета
+        /// </summary>
+        /// <param name="info">счет</param>
+        /// <param name="fullname">ФИО</param>
+        public static void Account(Info info, string fullname)
+        {
+            if (users.ContainsKey(fullname))
+            {
+                users[fullname].Add(info);
+            }
+            else
+            {
+                Console.WriteLine("Создайте пользователя");
+            }
+        }
+        /// <summary>
+        /// вывод информации о клиентах
+        /// </summary>
+        public static void PrintClients()
+        {
+            foreach (var user in users)
+            {
+                Console.WriteLine(user.Key);
+                foreach (Info info1 in user.Value)
+                {
+                    info1.PrintInfo();
+                }
+            }
+
+        }
+    }
+}
