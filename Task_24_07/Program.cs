aedd8ea80095a1714ec87943458dd1899ec72ae2
petrovich.kruglyash@mail.ru <petrovich.kruglyash@mail.ru>
2025-05-30 21:39:58 +0500

    занятие 24, задание 7

---
+++ Task_24_07/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Task_24_07
+{
+    class Program
+    {
+        // Реализуйте функцию, которая ищет заданное слово в текстовом файле и возвращает все строки, содержащиеэтослово (регистронезависимо)
+        static List<string> FindLinesWithWord(string path, string word)
+        {
+            List<string> result = new List<string>();
+            string line;
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.IndexOf(word, StringComparison.OrdinalIgnoreCase) >= 0)
+                    {
+                        result.Add(line);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        static void Main()
+        {
+            string filePath = "TextFile1.txt"; 
+            string searchWord = "молоко";    // Слово для поиска
+
+            if (File.Exists(filePath))
+            {
+                List<string> matchingLines = FindLinesWithWord(filePath, searchWord);
+
+                Console.WriteLine($"Строки с словом \"{searchWord}\":");
+                foreach (string line in matchingLines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Файл не найден.");
+            }
+
+        }
+    }
+}
