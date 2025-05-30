13869f679be522246efef96c390042eb9699d1d9
petrovich.kruglyash@mail.ru <petrovich.kruglyash@mail.ru>
2025-05-30 21:30:08 +0500

    занятие 24, задание 6

---
+++ Task_24_06/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Task_24_06
+{
+    internal class Program
+    { //Напишите метод, который принимает путь к файлу и возвращает количество строк в нем. ИспользуйтеStreamReader.//
+        static int CountLines(string path)
+        {
+            int count = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                while (reader.ReadLine() != null)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        static void Main()
+        {
+            string filePath = "strings.txt";
+            if (File.Exists(filePath))
+            {
+                int lines = CountLines(filePath);
+                Console.WriteLine("Количество строк: " + lines);
+            }
+            else
+            {
+                Console.WriteLine("Файл не найден.");
+            }
+        }
+    }
+}
